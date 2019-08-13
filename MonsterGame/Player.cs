265a598f245cf4fb84876cc5394f0ef4e7b1ee91
jrousseau <jrousseau@DEV-JROUSSEAU>
2019-08-13 08:54:31 +0200

    Player.cs

--- MonsterGame/Player.cs
+++ MonsterGame/Player.cs
@@ -5,8 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterGame
-{
+{ 
     class Player
     {
+        // The player is alive at the start of the game.
+        private bool isAlive = true;
+
+        // The number of easy monsters that have been killed.
+        public int EasyMonsterCounter { get; set; }
+
+        // The number of difficult monsters that have been killed.
+        public int DifficultMonsterCounter { get; set; }
+
+        // Life points on read only.
+        public int Life { get; }
+
+        // The name of the palyer.
+        public string Name { get; set; }
+
+        // The points won by the player while killing monsters.
+        public int Points { get; set; }
+
+        // If the life points are set to 0, the player dies.
+        public bool IsAlive
+        {
+            get
+            {
+                if (Life == 0)
+                {
+                    isAlive = false;
+                }
+                return isAlive;
+            }
+        }
+        
+        // Contructor which set life points to 150.
+        public Player()
+        {
+            this.Life = 150;
+        }
+
+        // The attacking method to kill monsters.
+        public void Attack(object monster)
+        {
+            //do stuff
+        }
     }
 }
--- MonsterGame/Program.cs
+++ MonsterGame/Program.cs
@@ -10,6 +10,7 @@ namespace MonsterGame
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
